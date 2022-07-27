5f08d6fc1ddee14585eb73c1ea69c0760a6fa9ea
Vlksju <xenya.vol2017@yandex.ru>
2022-07-27 20:59:27 +0500

    Solved task 2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа, которая выводит минимум и максимум из двух чисел");
+Console.WriteLine("Введите 2 числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine ());
+int number2 = Convert.ToInt32(Console.ReadLine ());
+
+if (number1 > number2) {
+    Console.Write("Максимум - ");
+    Console.WriteLine(number1);
+    Console.Write("Минимум - ");
+    Console.WriteLine(number2);
+}
+else {
+    Console.Write("Максимум - ");
+    Console.WriteLine(number2);
+    Console.Write("Минимум - ");
+    Console.WriteLine(number1);  
+}
