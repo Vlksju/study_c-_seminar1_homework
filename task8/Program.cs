14affb50f7bd60fc48d5fee437de9c5cdf6a5cb4
Vlksju <xenya.vol2017@yandex.ru>
2022-07-27 21:28:08 +0500

    Solved task 8

---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Программа, которая выводит все четные числа от 1 до N");
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+int count = 2;
+while (count <= number) {
+    Console.WriteLine(count);
+    count = count+2;}
