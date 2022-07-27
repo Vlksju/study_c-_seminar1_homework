0c1438abfaee2e141a7df9769ba053881c654063
Vlksju <xenya.vol2017@yandex.ru>
2022-07-27 21:19:29 +0500

    Solved task 6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа, которая определяет является ли число четным");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+if (number % 2 == 0) {
+    Console.WriteLine("четное");}
+
+ else {
+    Console.WriteLine("НЕчетное");}
+
+
+
